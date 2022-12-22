f001b7d238bced5a23daea8150b1bcafb90ade7f
Bradley Lyman <lyman.brad3211@gmail.com>
2022-12-22 17:22:27 -0500

    use Valtio to share state between p5 and react

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,26 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Sketch from "../lib/sketch";
 import * as appCssClasses from "./index.module.css";
-import MySketch from "./sketch";
+import MySketch, { store, BackgroundColor } from "./sketch";
+import { useSnapshot } from "valtio";
 
 function App() {
+  const snap = useSnapshot(store);
   return (
     <div>
+      <div className={appCssClasses.infoPanel}>
+        <h2>Point Count: {snap.pointCount}</h2>
+        <select
+          value={snap.background}
+          onChange={(event) => {
+            store.background = event.target.value as BackgroundColor;
+          }}
+        >
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="gray">Gray</option>
+        </select>
+      </div>
       <Sketch
         impl={new MySketch()}
         className={appCssClasses.mySketch}
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,6 +1,13 @@
 import P5Sketch from "../lib/P5Sketch";
 import P5 from "p5";
 
+import { proxy } from "valtio";
+
+export type BackgroundColor = "gray" | "blue" | "green";
+export const store = proxy<{ pointCount: number; background: BackgroundColor }>(
+  { pointCount: 0, background: "gray" }
+);
+
 export default class MySketch extends P5Sketch {
   points: { x: number; y: number }[] = [];
 
@@ -9,10 +16,11 @@ export default class MySketch extends P5Sketch {
   }
 
   draw(p: P5): void {
-    p.background(200);
+    p.background(store.background);
 
     if (p.mouseIsPressed) {
       this.points.push({ x: p.mouseX, y: p.mouseY });
+      store.pointCount = this.points.length;
     }
 
     for (let i = 1; i < this.points.length; i++) {
@@ -24,5 +32,6 @@ export default class MySketch extends P5Sketch {
 
   mousePressed = () => {
     this.points = [];
+    store.pointCount = this.points.length;
   };
 }
